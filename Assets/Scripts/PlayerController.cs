dc00b200b67285c10c918d2e82a4c981132564c0
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-08-16 20:26:47 +0200

    Colisiones por código y zonas pokemon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
+[RequireComponent(typeof(Animator))]
+
 public class PlayerController : MonoBehaviour
 {
     //Para controlar si el player se mueve o no en cada momento
@@ -15,13 +17,33 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] [Tooltip("Velocidad de movimiento")]
     private float speed = 1;
+
+    [SerializeField] [Tooltip("Capa/s a la/s que está/n asignado/s los Tilemap de objetos sólidos por colisión")]
+    private LayerMask solidObjectsLayer;
+    
+    [SerializeField] [Tooltip("Capa/s a la/s que está/n asignado/s los Tilemap de zonas de aparición de pokemon")]
+    private LayerMask pokemonLayer;
     
     //Para guardar los valores de los ejes x/y antes de transmitirlos al player
     private Vector2 input;
+    
+    //Animaciones del player
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         MovePlayer();
+    }
+
+    private void LateUpdate()
+    {
+        //Al finalizar todos los cálculos, establece si se debe reproducir una animación de movimiento o de idle
+        _animator.SetBool("IsMoving", isMoving);
     }
 
     private void MovePlayer()
@@ -41,16 +63,30 @@ public class PlayerController : MonoBehaviour
                 if (input.x != 0)
                     input.y = 0;
                 
-                //Se calcula la posición de destion (como va dirigida al
+                //Se asigna valor a la variable del animator que indicará tanto la animación que se debe reproducir al
+                //caminar como la dirección hacia la que quedará mirando al detenerse el personaje
+                _animator.SetFloat("MoveX", input.x);
+                _animator.SetFloat("MoveY", input.y);
+                
+                //Se calcula la posición de destino (como va dirigida al
                 //transform será un Vector3 aunque estemos en 2D)
                 Vector3 targetPosition = transform.position;
                 targetPosition.x += input.x;
                 targetPosition.y += input.y;
+                
+                /*Nota: este tipo de movimiento hará que el personaje se mueva siempre una unidad hacia alguno de los
+                 lados. Por ello, es importante no colocar al mismo en la posición x:0, y:0 de salida en la escena,
+                 ya que al finalizar cada movimiento se quedaría en medio de cada "casilla" de la grid, y por tanto
+                 se quedaría también en medio de puertas, obstáculos, etc... Lo ideal será que el player salga
+                 desde la posición x:0.5, y en el eje y una cantidad algo superior de forma que la parte de abajo,
+                 normalmente representada por una sombra aunque dependerá del artista, quede justo en el borde
+                 inferior de la "casilla" inicial del grid. En este caso concreto el valor es de y:0.7*/
 
                 //Se utilizará una corutina para evitar movimientos bruscos
                 //que surgirían si cada movimiento dura más de 1 frame,
                 //lo cual es lo habitual
-                StartCoroutine(MoveToward(targetPosition));
+                if(IsAvailable(targetPosition))//Solo si está disponible la posición de destino se moverá hacia ella
+                    StartCoroutine(MoveToward(targetPosition));
             }
         }
     }
@@ -85,5 +121,25 @@ public class PlayerController : MonoBehaviour
         transform.position = destination;
 
         isMoving = false; //Se finaliza el movimiento
+    }
+
+    /// <summary>
+    /// Comprueba si un punto está libre para caminar hacia él (no se va a colisionar con un
+    /// objeto que esté en las capas de colisión)
+    /// </summary>
+    /// <param name="target">Coordenadas del punto de destino</param>
+    /// <returns>true si el destino está libre / false en caso contrario</returns>
+    private bool IsAvailable(Vector3 target)
+    {
+        //Utiliza el motor de físicas 2D para hacer la comprobación. El método más adecuado es el que utiliza
+        //un círculo en vez de una caja, para intentar evitar colisiones con esquinas muy pequeñas. Comprueba si
+        //un círculo con centro en la posición a la que se desea caminar, con un radio ligeramente inferior a la
+        //mitad de la "caja" que representaría al player, se produciría colisión con la capa de objetos físicos
+        //Nota: para que funcione correctamente, el componente Composite Collider de la capa de objetos físicos
+        //debe tener la propiedad "Geometry Type" en "Polygons"
+        if (Physics2D.OverlapCircle(target, 0.2f, solidObjectsLayer) != null)
+            return false;
+
+        return true;
     }
 }
